5320068bb690dcc3788f9f2b8efb4c0035fa2aee
JD <jann.driessen@gmx.de>
2024-01-13 07:32:20 +0100

    add scale animation to gho button

--- shop/src/app/payment/components/gho-button.tsx
+++ shop/src/app/payment/components/gho-button.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 
 import { PrimaryButton } from '@/components/ui/button'
@@ -8,16 +9,18 @@ type GhoButtonProps = {
 
 export function GhoButton({ onClick }: GhoButtonProps) {
   return (
-    <PrimaryButton size={'lg'} onClick={onClick}>
-      <div className="flex items-center gap-2">
-        <Image
-          src="/assets/icon-light.svg"
-          width={36}
-          height={36}
-          alt="GHO icon"
-        />
-        <p>Pay with GHO</p>
-      </div>
-    </PrimaryButton>
+    <motion.button whileHover={{ scale: 1.06 }} whileTap={{ scale: 0.96 }}>
+      <PrimaryButton size={'lg'} onClick={onClick}>
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/icon-light.svg"
+            width={36}
+            height={36}
+            alt="GHO icon"
+          />
+          <p>Pay with GHO</p>
+        </div>
+      </PrimaryButton>
+    </motion.button>
   )
 }
