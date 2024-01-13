481c82df8a394d6c09acd98d41e4a354ba03edf4
JD <jann.driessen@gmx.de>
2024-01-13 11:58:16 +0100

    add items for payment popup

--- shop/src/app/payment/components/payment-popup.tsx
+++ shop/src/app/payment/components/payment-popup.tsx
@@ -33,14 +33,65 @@ const variants = {
   },
 }
 
+const variantsItems = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+}
+
 export const PaymentPopup = () => (
-  <motion.div className="background shadow-xl" variants={sidebar}>
+  <motion.div className="background shadow-xl p-8" variants={sidebar}>
     <motion.ul variants={variants}>
-      {itemIds.map((i) => (
-        <div />
-      ))}
+      <PayHeadline label="Pay 129.159 GHO" key={0} />
+      <QRCode code={'0x0'} key={1} />
+      <div className="mt-6">
+        <Text label="To pay, scan the code inside the app" key={2} />
+      </div>
+      <div className="mt-8">
+        <Text label="or..." key={3} />
+      </div>
     </motion.ul>
   </motion.div>
 )
 
-const itemIds = [0, 1, 2, 3, 4]
+export const PayHeadline = ({ label }: { label: string }) => {
+  return (
+    <motion.li variants={variantsItems}>
+      <h2 className="font-bold text-4xl text-center mt-8">{label}</h2>
+    </motion.li>
+  )
+}
+
+export const QRCode = ({ code }: { code: string }) => {
+  return (
+    <motion.li variants={variantsItems}>
+      <div className="bg-white rounded-xl p-8 mt-8">
+        <img
+          src="/assets/qr-code-temp.png"
+          width={300}
+          height={300}
+          alt="GHO QR code"
+        />
+      </div>
+    </motion.li>
+  )
+}
+
+export const Text = ({ label }: { label: string }) => {
+  return (
+    <motion.li variants={variantsItems}>
+      <p className="font-semibold text-3xl text-center">{label}</p>
+    </motion.li>
+  )
+}
