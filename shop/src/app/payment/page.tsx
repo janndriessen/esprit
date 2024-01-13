5e2d7fb2f382189f749d37e9734735dff30637d1
JD <jann.driessen@gmx.de>
2024-01-13 07:02:19 +0100

    add gho button

--- shop/src/app/payment/page.tsx
+++ shop/src/app/payment/page.tsx
@@ -1,9 +1,35 @@
+'use client'
+
+import Image from 'next/image'
+
 import { PrimaryButton } from '@/components/ui/button'
 
 export default function Payment() {
+  const onClickPay = () => {
+    console.log('open payment')
+  }
   return (
     <div className="flex items-center justify-center h-screen">
-      <PrimaryButton>Hello</PrimaryButton>
+      <GhoButton onClick={onClickPay} />
     </div>
+  )
+}
+
+type GhoButtonProps = {
+  onClick: () => void
+}
+function GhoButton({ onClick }: GhoButtonProps) {
+  return (
+    <PrimaryButton size={'lg'} onClick={onClick}>
+      <div className="flex items-center gap-2">
+        <Image
+          src="/assets/icon-light.svg"
+          width={36}
+          height={36}
+          alt="GHO icon"
+        />
+        <p>Pay with GHO</p>
+      </div>
+    </PrimaryButton>
   )
 }
--- shop/src/components/ui/button.tsx
+++ shop/src/components/ui/button.tsx
@@ -5,11 +5,11 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-full text-lg font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        default: 'bg-primary text-primary-foreground hover:bg-primary',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        default: 'h-10 px-4 py-4',
+        sm: 'h-9 rounded-full px-4 py-4',
+        lg: 'h-11 rounded-full px-4 py-4',
         icon: 'h-10 w-10',
       },
     },
