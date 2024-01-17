6bd802e0cb0061b77e547114c45309a6f42e5666
CormacDaly <cormacdaly24@gmail.com>
2024-01-17 11:05:21 +0000

    refund-button created and added to recent-sales

---
+++ shop/src/app/dashboard/components/refund-button.tsx
@@ -0,0 +1,9 @@
+import { PrimaryButton as Button } from '@/components/ui/button'
+
+export function RefundButton() {
+    return(
+        <Button className="text-xs ml-4 transition:colors hover:text-secondary">Refund</Button>
+    )
+
+
+}
