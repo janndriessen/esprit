3ac80d0ffcdbe6cf2604d45c2552579d1f33094e
CormacDaly <cormacdaly24@gmail.com>
2024-01-17 13:22:04 +0000

    removed reference to light and dark, and missing file addresses

--- shop/src/app/dashboard/page.tsx
+++ shop/src/app/dashboard/page.tsx
@@ -27,22 +27,6 @@ export const metadata: Metadata = {
 export default function DashboardPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="hidden flex-col md:flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
