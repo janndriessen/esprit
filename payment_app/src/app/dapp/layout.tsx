bd72c2a59700c3ba26d642dcb4b6d319f3edb5ed
JD <jann.driessen@gmx.de>
2024-01-19 14:44:49 +0100

    start styling main screen

--- payment_app/src/app/dapp/layout.tsx
+++ payment_app/src/app/dapp/layout.tsx
@@ -1,22 +1,19 @@
 "use client";
 import Navigationbar from "@/components/Navigationbar";
-import {Web3AuthProvider} from "@/provider/Web3AuthProvider";
+import { Web3AuthProvider } from "@/provider/Web3AuthProvider";
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-        return (
-            <main>
-                <div className="flex justify-center items-center fixed top-0 bg-black text-white w-screen h-16 z-50">
-                    <div className="text-3xl">Esprit</div>
-                </div>
-                <Web3AuthProvider>
-                    {children}
-                </Web3AuthProvider>
-                <Navigationbar className="fixed bottom-0 bg-black text-white w-screen h-16"/>
-            </main>
-        );
-
+  return (
+    <main>
+      {/* <div className="flex justify-center items-center fixed top-0 bg-black text-white w-screen h-16 z-50">
+        <div className="text-3xl">Esprit</div>
+      </div> */}
+      <Web3AuthProvider>{children}</Web3AuthProvider>
+      <Navigationbar className="fixed bottom-0 bg-black text-white w-screen h-16" />
+    </main>
+  );
 }
--- payment_app/src/app/dapp/page.tsx
+++ payment_app/src/app/dapp/page.tsx
@@ -1,19 +1,12 @@
 "use client";
+
+import { Card } from "@/components/ui/card";
 import UserInfo from "@/components/user/UserInfo";
 
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-
-
 export default function Home() {
-    return (
-        <div  className="flex flex-col min-h-screen items-center justify-center">
-            <Card className="flex flex-col items-center justify-evenly">
-                <UserInfo className="p-10" />
-            </Card>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen items-center p-8">
+      <UserInfo />
+    </div>
+  );
 }
