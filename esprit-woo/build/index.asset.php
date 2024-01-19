331ad8ac5b11160bc56077db4b547e028f5bf19c
brianonchain <jonwayhuang@gmail.com>
2024-01-20 01:57:55 +0800

    wc plugin tested

--- esprit-woo/build/index.asset.php
+++ esprit-woo/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'caa9ae0720f92f20607c');
+<?php return array('dependencies' => array(), 'version' => '1f6fbcae425714b61ad3');
--- esprit-woo/src/index.js
+++ esprit-woo/src/index.js
@@ -1,8 +1,9 @@
 const QRCode = require("qrcode");
 const { v4: uuidv4 } = require("uuid");
 const Pusher = require("pusher-js");
+const { GelatoRelay } = require("@gelatonetwork/relay-sdk");
 
-const pusher = new Pusher("a29a22e46d485f5f2038", { cluster: "ap3" });
+const pusher = new Pusher("56bc83149af81d0da2d4", { cluster: "ap1" });
 pusher.connection.bind("error", (e) => {
   console.error("Pusher connection error:", e);
 });
@@ -15,25 +16,62 @@ jQuery(document).ready(($) => {
 
       $(document).ajaxComplete(async () => {
         $("#place_order").hide();
-        const dataURL = await QRCode.toDataURL(`paymentId=${paymentId}&amount=0.009678&address=${data.address}&amountUSD=${data.amount}`);
+        const opts = {
+          margin: 0,
+        };
+        // example: paymentId=e6844694-b556-4133-a501-dafb915beee9&address=0x0ad11e8600b488aeF8F5E591edf32927c586883b&amount=200
+        const dataURL = await QRCode.toDataURL(`paymentId=${paymentId}&address=${data.address}&amount=${data.amount}`, opts);
         // If the qrcode element exists, then don't append it. Needed or multiple qrcodes will get appended.
-        if (!$(".esprit-qrcode").length) {
-          $(".esprit-container").append(`<img src=${dataURL} class="esprit-qrcode" />`);
+        if (!$(".esprit-container-container").length) {
           $(".esprit-container").append(
-            `<div class="esprit-text-container"><div class="esprit-text-one">Pay with Esprit and earn an instant 2% cashback!</div><div class="esprit-text-two">To pay, scan the QR code with your Esprit App and confirm the payment. Leave this window open during payment.</div></div>`
+            `<div class="esprit-container-container"><img src=${dataURL} class="esprit-qrcode" /><div class="esprit-text-container"><div class="esprit-text-one">Pay with Esprit and earn an instant 2% cashback!</div><div class="esprit-text-two">To pay, scan the QR code with your Esprit App and confirm the payment. Leave this window open during payment.</div></div></div>`
           );
         }
         // If not subscribed, then subcribe. Needed or will execute multiple subscriptions and callback will be executed multiple times.
-        // subscribe to Pusher
-        let channels = pusher.allChannels();
-        if (!channels.includes(data.paymentId)) {
-          const channel = pusher.subscribe("my-channel"); //data.paymentId
-          channel.bind("my-event", async (message) => {
-            alert(JSON.stringify(message));
-            pusher.unsubscribe("my-channel"); //data.paymentId
-            $("#place_order").show();
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            $("#place_order").click();
+        let channels = await pusher.allChannels();
+        let channelNames = channels.map((i) => i.name);
+        if (!channelNames.includes(paymentId)) {
+          console.log("pusher channel:", paymentId);
+          const channel = pusher.subscribe(paymentId);
+          channel.bind("payment-submitted", async (pusherData) => {
+            let taskId = pusherData.message;
+            console.log("pusher taskId:", taskId); // indicate Pusher message received; this should appear once to indicate we only subscribed once
+            await pusher.unsubscribe(paymentId);
+
+            // show process payment message
+            $(".esprit-container-container").hide();
+            $(".esprit-container").append(
+              '<div class="esprit-processing-container"><div class="esprit-processing-one">Payment is processing...</div><div class="esprit-processing-two">(Please do not close this window)</div></div>'
+            );
+            // poll GelatoRelay for completed txn
+            const relay = new GelatoRelay();
+            const taskStatusPromise = new Promise((resolve, reject) => {
+              const maxRetry = 100;
+              let retryNum = 0;
+              const interval = setInterval(async () => {
+                retryNum++;
+                if (retryNum > maxRetry) {
+                  clearInterval(interval);
+                  reject("Max retry reached");
+                }
+                const taskStatus = await relay.getTaskStatus(taskId);
+                console.log("Task Status", taskStatus);
+                if (taskStatus?.taskState == "ExecSuccess") {
+                  clearInterval(interval);
+                  resolve(taskStatus);
+                }
+              }, 500);
+            });
+            const taskStatus = await taskStatusPromise;
+            console.log("taskStatus.taskState:", taskStatus.taskState);
+
+            // click Place Order button if success
+            // TODO: if taskState == "Cancelled", then display error modal
+            if (taskStatus.taskState == "ExecSuccess") {
+              $("#place_order").show();
+              await new Promise((resolve) => setTimeout(resolve, 1000)); // seems neccessary
+              $("#place_order").click();
+            }
           });
         }
       });
