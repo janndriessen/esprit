f4307d65bb073d4ff7a64b665d3447ebde67b8d3
JD <jann.driessen@gmx.de>
2024-01-19 14:14:09 +0100

    style intro page

--- payment_app/src/app/page.tsx
+++ payment_app/src/app/page.tsx
@@ -1,32 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+import { CardDescription, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen items-center justify-center">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Esprit</CardTitle>
-                    <CardDescription>
-                        Easy Payments with QR Codes
-                    </CardDescription>
-                </CardHeader>
-
-                <CardContent>
-                    <div className="flex justify-evenly items-center">
-                        <Button asChild variant="default">
-                            <Link href="/dapp">Log In</Link>
-                        </Button>
-                    </div>
-                </CardContent>
-            </Card>
-        </main>
-    );
+  return (
+    <main className="background flex min-h-screen items-center justify-center p-20">
+      <div className="flex-col items-center align-center">
+        <Image
+          alt="Index Coop Logo"
+          src={"/logo.png"}
+          height={128}
+          width={128}
+          className="mx-auto"
+        />
+        <CardTitle className="text-center text-4xl mt-6">ESPRIT</CardTitle>
+        <CardDescription className="text-center mt-2 text-xl">
+          Payments made easy
+        </CardDescription>
+        <div className="flex justify-evenly items-center mt-8">
+          <Button asChild variant="default">
+            <Link href="/dapp">Sign In</Link>
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
