d942a69adc5910a5e5f8d419464077380c1d1eb6
test <test@gmail.com>
2024-01-20 11:57:55 +0800

    Set up pwa

--- payment_app/src/app/layout.tsx
+++ payment_app/src/app/layout.tsx
@@ -7,6 +7,11 @@ export const metadata = {
     title: "Esprit",
     description: "Easy stablecoin payments",
     viewport: "width=device-width, user-scalable=no",
+    manifest: "/manifest.json",
+    icons: [
+    { rel: "apple-touch-icon", url: "/icon-192x192.png" },
+    { rel: "icon", url: "/icon-192x192.png" },
+  ],
 };
 
 export default function RootLayout({
